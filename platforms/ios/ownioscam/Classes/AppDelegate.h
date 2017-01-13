504742a17b4ec03f9153837aae7d8e98706f729a
WisemenDevelopment <raaj19091@gmail.com>
2017-01-13 14:50:16 +0530

    Update AppDelegate.h

--- platforms/ios/ownioscam/Classes/AppDelegate.h
+++ platforms/ios/ownioscam/Classes/AppDelegate.h
@@ -35,7 +35,7 @@
 // edit ownioscam-Info.plist to add a protocol
 // a simple tutorial can be found here :
 // http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
-
+//
 @property (nonatomic, strong) IBOutlet UIWindow* window;
 @property (nonatomic, strong) IBOutlet CDVViewController* viewController;
 
